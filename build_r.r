4dbaf20ab3a7add72ce72cec241655f9974535af
ciplogic <ciprian.mustiata@yahoo.com>
2015-08-20 18:13:41 +0300

    Dependency of data.table is included. Also, the build script is updated to work easier with new R runtime.

--- build_r.r
+++ build_r.r
@@ -1,7 +1,26 @@
-require(roxygen2)
-require(devtools)
+quickRequire <- function(packageName){
+	if(!require(packageName)){
+		install.packages(packageName, repos="http://cran.us.r-project.org")
+		require(packageName)
+	}
+  
+}
+
+quickRequire("roxygen2")
+quickRequire("devtools")
+quickRequire("data.table")
+
+installBinaryHttr <- function(fdkRLibPackage){
+  basicUrl = "https://github.com/SoftFx/FdkRLib/raw/master/dist/"
+  fullUrl = paste(basicUrl, fdkRLibPackage, sep = "")
+  download.file(fullUrl,destfile = fdkRLibPackage, method = "libcurl")
+  
+  install.packages(fdkRLibPackage, repos = NULL, type = "source", dependencies = TRUE)
+  file.remove(fdkRLibPackage)
+}
+installBinaryHttr("rClr_0.7-4.zip")
+
+
 setwd("RPackage")
 devtools::document(roclets=c('rd', 'collate', 'namespace'))
 packPath <- devtools::build(binary = TRUE, args = c('--preclean'))
-
-
