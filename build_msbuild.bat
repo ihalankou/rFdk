030b6a24b906640ddb85439ce3c0de6cb3b73795
ciplogic <ciprian.mustiata@yahoo.com>
2015-12-11 12:03:27 +0200

    Add a build_msbuild batch file which doesn't require msbuild.

---
+++ build_msbuild.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set FrameworkDir="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\"
+set R_Exe=""D:\apps\R\R-3.2.2\bin\R.exe""
+
+
+cd Fdk2R
+%FrameworkDir%\msbuild.exe RFdk.sln /target:x86 /p:Configuration=Release /maxcpucount /fl /flp:Verbosity=Normal;LogFile=Log /clp:NoItemAndPropertyList /verbosity:n /nologo
+cd ..
+%R_Exe% CMD BATCH build_r.r
+move /Y rFdk_1.0.20151204.zip dist 
