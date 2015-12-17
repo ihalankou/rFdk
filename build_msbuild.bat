06bd70ad79d671e828a1069f927127f857e218b1
ciplogic <ciprian.mustiata@yahoo.com>
2015-12-17 14:08:12 +0200

    Added code to validate tools.

--- build_msbuild.bat
+++ build_msbuild.bat
@@ -3,6 +3,17 @@
 set MsBuildFilePath="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 set R_Exe="C:\Program Files\R\R-3.2.1\bin\R.exe"
 
+if not exist %MsBuildFilePath% (
+  echo Please set variable MsBuildFilePath to point to your MSBuild tool
+  pause
+)
+
+
+if not exist %R_Exe% (
+  echo Please set variable R_Exe to point to your R distribution
+  pause
+)
+
 
 cd Fdk2R
 "%MsBuildFilePath%" RFdk.sln /target:Rebuild /p:Platform=x86 /p:Configuration=Release /maxcpucount /fl /flp:Verbosity=Normal;LogFile=build.Log /clp:NoItemAndPropertyList /verbosity:n /nologo
