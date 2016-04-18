eb0de1788658686153ccca96515582ec5e449d4c
ihalankou <ihar.halankou@soft-fx.lv>
2016-04-18 15:35:59 +0300

    Add tz="GMT"
    Add tz="GMT"

--- examples/sample_bar_pairs.R
+++ examples/sample_bar_pairs.R
@@ -1,7 +1,7 @@
-endTime <- as.POSIXlt(Sys.time())
+endTime <- as.POSIXlt(Sys.time(), , tz="GMT")
 startTime <- as.POSIXct(0, origin=ISOdatetime(2015,3,2,11,16,16))
-st1 <- as.POSIXct(startTime)
-et1 <- as.POSIXct(endTime)
+st1 <- as.POSIXct(startTime, tz="GMT")
+et1 <- as.POSIXct(endTime, tz="GMT")
 
 barPairs = ttFeed.BarHistory(symbol = "EURUSD", barPeriodStr = "M1", startTime = st1)
 
