d99fa6eb23dfd08cddd29fb7ab6802380e63379f
ihalankou <ihar.halankou@soft-fx.lv>
2016-04-18 14:32:44 +0300

    Update sample_trades.R
    Update sample_trades.R

--- examples/sample_trades.R
+++ examples/sample_trades.R
@@ -2,6 +2,11 @@
 # Connect to server before running the sample
 # ttConnect()
 
-# gets all trades you have with the current account
-trades = ttTrades()
-View(trades)
+# gets all open oredrs you have with the current account
+tradesOpen = ttTrade.Open()
+View(tradesOpen)
+
+# gets all history trades you have with the current account 
+6 tradesHistory = ttTrade.History()
+7 View(tradesHistory) 
+
