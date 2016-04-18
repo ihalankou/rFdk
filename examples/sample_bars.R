f5baed134e0f2183c593b4e69a5cf703abfda34e
ihalankou <ihar.halankou@soft-fx.lv>
2016-04-18 14:27:53 +0300

    Update sample_bars.R
    Update sample_bars.R

--- examples/sample_bars.R
+++ examples/sample_bars.R
@@ -2,7 +2,7 @@
 # Connect to server before running the sample
 # ttConnect()
 ttConnect()
-bars = ttBarsHistory("EURUSD", "Bid", "H1")
+bars = ttFeed.BarHistory("EURUSD", "Bid", "H1")
 
 boxplot(bars$highs)
 
@@ -14,9 +14,9 @@ startTime <- strptime("20/3/2 11:16:16.683", "%d/%m/%y %H:%M:%OS")
 st1 <- as.POSIXct(startTime)
 et1 <- as.POSIXct(endTime)
 
-quotes <- ttQuotes("EURUSD", st1, et1, 1)
+quotes <- ttFeed.TickBestHistory("EURUSD", st1, et1)
 plot(quotes$ask, type="o")
 
-bars = ttBarsHistory(symbol = "EURUSD", barPeriodStr = "M15", priceTypeStr = "Ask", barCount = 100000000)
+bars = ttFeed.BarHistory(symbol = "EURUSD", barPeriodStr = "M15", priceTypeStr = "Ask", barCount = 100000000)
 
-barRange = ttBarsHistory(symbol = "EURUSD", barPeriodStr = "M1", priceTypeStr = "Ask", startTime = startTime)
+barRange = ttFeed.BarHistory(symbol = "EURUSD", barPeriodStr = "M1", priceTypeStr = "Ask", startTime = startTime)
