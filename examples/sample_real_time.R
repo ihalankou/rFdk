e5ddd392b1ed873c69de5b4a219ab9776d9b4afe
ihalankou <ihar.halankou@soft-fx.lv>
2016-04-18 14:30:25 +0300

    Update sample_real_time.R
    Update sample_real_time.R

--- examples/sample_real_time.R
+++ examples/sample_real_time.R
@@ -1,8 +1,8 @@
 ttConnect()
-idMonitor <- ttQuotesSubscribe('EURUSD', 2)
+idMonitor <- ttFeed.Subscribe('EURUSD', 2)
 # Wait for some time before running this line
 # it can be run multiple times and the snapshot reflects
-snapshot <- ttQuotesLevel2(idMonitor)
+snapshot <- ttFeed.GetLevel2(idMonitor)
 View(snapshot)
 
-ttQuotesUnsubscribe(idMonitor)
+ttFeed.Unsubscribe(idMonitor)
