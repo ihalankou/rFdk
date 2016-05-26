54962e1c2a2200eeacb24920df8471c7bd5521d9
ihalankou <ihar.halankou@soft-fx.lv>
2016-05-26 12:55:08 +0300

    Update sample_bar_pairs.R

--- examples/sample_bar_pairs.R
+++ examples/sample_bar_pairs.R
@@ -1,4 +1,4 @@
-endTime <- as.POSIXlt(Sys.time(), , tz="GMT")
+endTime <- as.POSIXlt(Sys.time(), tz="GMT")
 startTime <- as.POSIXct(0, origin=ISOdatetime(2015,3,2,11,16,16))
 st1 <- as.POSIXct(startTime, tz="GMT")
 et1 <- as.POSIXct(endTime, tz="GMT")
